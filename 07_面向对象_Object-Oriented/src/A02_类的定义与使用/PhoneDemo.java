4fa6f2cbc317eac65ef7cc9a9b82249031194d52
George <buubeebuu@126.com>
2021-04-05 00:10:04 +0800

    类的定义与使用

---
+++ 07_面向对象_Object-Oriented/src/A02_类的定义与使用/PhoneDemo.java
@@ -0,0 +1,20 @@
+package A02_类的定义与使用;
+
+public class PhoneDemo {
+    public static void main(String[] args) {
+        //创建对象
+        Phone p = new Phone();
+
+        //使用成员变量
+        p.brand = "小米";
+        p.price = 2999;
+
+        System.out.println(p.brand);
+        System.out.println(p.price);
+
+        //使用成员方法
+        p.call();
+        p.sendMessage();
+
+    }
+}
