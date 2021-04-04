ef3f89258364069d287aa00f16e1c86e363a86e4
George <buubeebuu@126.com>
2021-04-04 22:33:56 +0800

    数组初始化之动态初始化

---
+++ 04_数组_Array/src/D07_数组初始化之动态初始化/ArrayDemo.java
@@ -0,0 +1,16 @@
+package D07_数组初始化之动态初始化;
+/*
+    动态初始化
+        先声明和定义数组，然后再赋值
+ */
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        int[] arr = new int[3]; // 声明
+
+        arr[0] = 1;		//初始化
+        arr[1] = 2;
+        arr[2] = 3;
+
+    }
+}
