06bd781f6894278686067f27773a75f75b469b65
George <buubeebuu@126.com>
2021-04-04 22:03:13 +0800

    顺序结构

---
+++ 03_分支语句/src/B01_顺序结构/OrderDemo01.java
@@ -0,0 +1,16 @@
+package B01_顺序结构;
+
+/*
+	顺序结构
+*/
+public class OrderDemo01 {
+    public static void main(String[] args) {
+        System.out.println("开始");
+
+        System.out.println("语句A");
+        System.out.println("语句B");
+        System.out.println("语句C");
+
+        System.out.println("结束");
+    }
+}
