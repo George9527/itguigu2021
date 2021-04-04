fab338294dc03e0b2b83012b05340f191d2bd6c7
George <buubeebuu@126.com>
2021-04-05 00:11:49 +0800

    类的内存图

---
+++ 07_面向对象_Object-Oriented/src/A04_对象内存图/StudentDemo02.java
@@ -0,0 +1,41 @@
+package A04_对象内存图;
+
+public class StudentDemo02 {
+    public static void main(String[] args) {
+        //创建第一个对象
+        Student l = new Student();
+        //输出对象内存地址
+        //System.out.println(l);
+
+        //使用对象
+        //输出对象未赋值前内容
+        //System.out.println(l.name + "," + l.age);
+
+        l.name = "林青霞";
+        l.age = 30;
+
+        System.out.println(l.name + "," + l.age);
+
+        //使用成员方法
+        l.study();
+        l.doHomework();
+
+        //创建第二个对象
+        Student z = new Student();
+        //输出对象内存地址
+        //System.out.println(z);
+
+        //使用对象
+        //输出对象未赋值前内容
+        //System.out.println(z.name + "," + z.age);
+
+        z.name = "张曼玉";
+        z.age = 35;
+
+        System.out.println(z.name + "," + z.age);
+
+        //使用成员方法
+        z.study();
+        z.doHomework();
+    }
+}
