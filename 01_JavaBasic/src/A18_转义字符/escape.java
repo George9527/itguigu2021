036aa2e67bc8cfa5f53810808c895081f8f64437
George <buubeebuu@126.com>
2021-04-04 20:18:30 +0800

    转义字符/制表符

---
+++ 01_JavaBasic/src/A18_转义字符/escape.java
@@ -0,0 +1,17 @@
+package A18_转义字符;
+/*
+    转义字符 制表符
+ */
+public class escape {
+    /*
+        \n：换行
+        \r：回车
+        \t：Tab键
+        \\：\
+        \"："
+        \'：'
+        \b：删除键Backspace
+        System.out.println('\\');
+        System.out.println("hello\tworld\njava");
+     */
+}
