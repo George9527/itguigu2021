abdc924bddb5bc731c8edc897462284357845f67
George <buubeebuu@126.com>
2021-04-04 15:43:24 +0800

    数据类型自动转换的练习题

---
+++ 01_JavaBasic/src/A08_数据类型转换_Conversion/ConversionTest06.java
@@ -0,0 +1,38 @@
+package A08_数据类型转换_Conversion;
+/*
+    数据类型自动转换的练习题
+ */
+public class ConversionTest06 {
+    public static void main(String[] args) {
+
+        // 1、练习题：判断如下代码是否编译通过，如果能，结果是多少？
+
+//        short s1 = 120;
+//        short s2 = 8;
+////        short s3 = s1 + s2;
+//
+//        short s1 = 120;
+//        short s2 = 8;
+//        byte s3 = (byte)(s1 + s2);
+//
+//        char c1 = '0';
+//        char c2 = '1';
+//        char c3 = c1 + c2;
+//
+//        char c1 = '0';
+//        char c2 = '1';
+//        System.out.println(c1 + c2);
+//
+//        int i = 4;
+//        long j = 120; //因为右边120默认是int类型，int的值赋值给long类型是可以的，会自动类型转换，但是要求这个int值不能超过int的存储范围，如果超过int的存储范围必须加L.
+//        double d = 34;
+//        float f = 1.2;//因为右边1.2默认是double类型，double的值是不能直接赋值给float的，要么加F要么使用强制类型转换。
+//
+//        System.out.println(i + j + d + f);//最后是double
+//
+//        int i = 1;
+//        int j = 2;
+//        double d = i/j;
+//        System.out.println(d);
+    }
+}
