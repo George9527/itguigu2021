6705c5b60e5b2e0b05f653cda8c74f70845ee37f
George <buubeebuu@126.com>
2021-04-04 22:18:59 +0800

    数组初始化之静态初始化

---
+++ 04_数组_Array/src/D06_数组初始化之静态初始化/ArrayDemo.java
@@ -0,0 +1,16 @@
+package D06_数组初始化之静态初始化;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        //定义数组
+        int[] arr = {1,2,3};
+
+        //输出数组名(地址值)
+        System.out.println(arr);
+
+        //输出数组中元素
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+    }
+}
