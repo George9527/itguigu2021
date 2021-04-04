3929f5f25bd963e4cec1d0431e4a280fc48c5971
George <buubeebuu@126.com>
2021-04-05 00:16:33 +0800

    构造的方法与使用

---
+++ 07_面向对象_Object-Oriented/src/A13_构造方法/StudentDemo.java
@@ -0,0 +1,12 @@
+package A13_构造方法;
+
+/*
+    测试类
+ */
+public class StudentDemo {
+    public static void main(String[] args) {
+        //创建对象
+        Student s = new Student();
+        s.show();
+    }
+}
