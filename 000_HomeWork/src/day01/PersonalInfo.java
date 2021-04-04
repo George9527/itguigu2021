fee416ba2a3725dcf7c1687d74893f04149e39ae
George <buubeebuu@126.com>
2021-04-04 14:01:24 +0800

    Day01—使用print打印个人信息

---
+++ 000_HomeWork/src/day01/PersonalInfo.java
@@ -0,0 +1,21 @@
+package day01;
+// 声明一个类：用来打印自我介绍
+public class PersonalInfo {
+    // 声明一个main函数
+    public static void main(String[] args){
+
+        // 使用打印语言输出信息
+        System.out.println("名字：封捷");
+        System.out.println("艺名：彦祖");
+        System.out.println("老家：河北张家口");
+        System.out.println("户口：深圳");
+        System.out.println("身高：178cm");
+        System.out.println("体重：187斤");
+        System.out.println("婚姻：已婚");
+        System.out.println("手机：13242033155");
+        System.out.println("微信：13242033155");
+        System.out.println("学历：本科");
+        System.out.println("学校：山西大同大学02级");
+        System.out.println("专业：网络工程");
+    }
+}
