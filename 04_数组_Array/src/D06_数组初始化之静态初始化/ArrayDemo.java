7384a7c1f4daa1e92d81a4cc9ec230cbce495c48
George <buubeebuu@126.com>
2021-04-04 22:23:52 +0800

    数组初始化之静态初始化

--- 04_数组_Array/src/D06_数组初始化之静态初始化/ArrayDemo.java
+++ 04_数组_Array/src/D06_数组初始化之静态初始化/ArrayDemo.java
@@ -2,6 +2,9 @@ package D06_数组初始化之静态初始化;
 
 public class ArrayDemo {
     public static void main(String[] args) {
+
+        // 方法一
+
         //定义数组
         int[] arr = {1,2,3};
 
@@ -12,5 +15,15 @@ public class ArrayDemo {
         System.out.println(arr[0]);
         System.out.println(arr[1]);
         System.out.println(arr[2]);
+
+        // 方法二
+
+        int[] arr2 = new int[] {1,2,3};		//正确
+//        将其拆开的写法：
+        int[] arr3;
+        arr3 = new int[] {1,2,3};		//正确
+
+//        int[] arr2;
+//        arr2 = new int[3]{1,2,3};		//错误
     }
 }
