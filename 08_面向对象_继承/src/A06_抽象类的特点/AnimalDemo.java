4daa952e2882a4711bbeb5e8e3b64ef1afef5ea5
George <buubeebuu@126.com>
2021-04-07 20:23:32 +0800

    抽象类的特点

--- 08_面向对象_继承/src/A06_抽象类的特点/AnimalDemo.java
+++ 08_面向对象_继承/src/A06_抽象类的特点/AnimalDemo.java
@@ -6,10 +6,14 @@ package A06_抽象类的特点;
 public class AnimalDemo {
     public static void main(String[] args) {
 
-        //抽象类如何创建对象
+        // 如何创建对象
         Animal a = new Cat();
         a.eat();
         a.sleep();
+
+        // Dog 是一个抽象类 抽象类不能建对象
+//        Animal d = new Dog();
+//        Dog dog = new Dog();
 
     }
 }
