a180baad4a51e6ee5c39c33ee215cfe60d27df41
George <buubeebuu@126.com>
2021-04-07 19:59:55 +0800

    抽象类概述

---
+++ 08_面向对象_继承/src/A04_抽象类概述/Animal.java
@@ -0,0 +1,20 @@
+package A04_抽象类概述;
+/*
+    任务：
+        新建一个抽象类和抽象方法
+
+    小结：
+        抽象方法必须在抽象类中
+
+    关键词：
+        abstract
+        方法体不给具体方法为抽象方法
+
+ */
+
+//定义为抽象类
+public abstract class Animal {
+
+    // 定义为抽象方法
+    public abstract void eat();
+}
