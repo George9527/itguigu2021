4247f43258045318b71865dc723e74eefb863504
George <buubeebuu@126.com>
2021-04-07 20:34:35 +0800

    抽象类的成员特点

---
+++ 08_面向对象_继承/src/A09_抽象类的成员特点/Animal.java
@@ -0,0 +1,38 @@
+package A09_抽象类的成员特点;
+/*
+    抽象类
+ */
+
+public abstract class Animal {
+
+    private int age = 20;
+    private String home = "河北";
+
+    // final 修饰符 将该变量注释为常量
+    private final String city = "北京";
+
+    // 给抽象类新建具体方法
+    public void show() {
+        //给 age 重新赋值
+        age = 40;
+        System.out.println(age);
+
+        // city = "上海"; --- 会报错
+        System.out.println(city);
+
+        //给 home 重新赋值
+        home = "湖南";
+        System.out.println(home);
+    }
+
+    //新建抽象方法
+    public abstract void eat();
+
+    //无参构造方法
+    public Animal() {}
+
+    //带参构造方法
+    public Animal(int age) {
+        this.age = age;
+    }
+}
---
+++ 08_面向对象_继承/src/A09_抽象类的成员特点/AnimalDemo.java
@@ -0,0 +1,9 @@
+package A09_抽象类的成员特点;
+
+public class AnimalDemo {
+    public static void main(String[] args) {
+        Animal a = new Cat();
+        a.eat();
+        a.show();
+    }
+}
