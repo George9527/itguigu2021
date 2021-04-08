5f4159280bd1564118c566ca53526489944dc6d7
George <buubeebuu@126.com>
2021-04-08 13:49:10 +0800

    多态的概述

---
+++ 09_面向对象_多态/src/A01_多态概述/Animal.java
@@ -0,0 +1,9 @@
+package A01_多态概述;
+
+public class Animal {
+
+    public void eat() {
+        System.out.println("动物吃东西");
+    }
+
+}
---
+++ 09_面向对象_多态/src/A01_多态概述/Cat.java
@@ -0,0 +1,9 @@
+package A01_多态概述;
+
+public class Cat extends Animal{
+
+    @Override
+    public void eat() {
+        System.out.println("猫吃鱼");
+    }
+}
