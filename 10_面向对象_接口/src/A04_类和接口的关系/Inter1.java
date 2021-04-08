8a7b0a65382d3a8660d941f46b2632834a91d85a
George <buubeebuu@126.com>
2021-04-08 15:18:37 +0800

    类和接口的关系

---
+++ 10_面向对象_接口/src/A04_类和接口的关系/Inter1.java
@@ -0,0 +1,4 @@
+package A04_类和接口的关系;
+
+public interface Inter1 {
+}
---
+++ 10_面向对象_接口/src/A04_类和接口的关系/InterImpl.java
@@ -0,0 +1,6 @@
+package A04_类和接口的关系;
+
+// 一个类能在继承来一个类时使用多个接口
+// 接口与接口之间 是继承关系 还可以是多继承
+public class InterImpl extends Object implements Inter1,Inter2,Inter3{
+}
