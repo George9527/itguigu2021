699660757fa179ffc93f2ed1b56c1e9881131395
George <buubeebuu@126.com>
2021-04-11 21:08:41 +0800

    成员属性调用的优先级

--- 10_面向对象_接口/src/A14_成员属性调用的优先级/Outer.java
+++ 10_面向对象_接口/src/A14_成员属性调用的优先级/Outer.java
@@ -1,10 +1,13 @@
 package A14_成员属性调用的优先级;
 /*
     就近原则，当调用内部类时，优先使用内部类里面的变量
+    局部内部类，使用局部变量，变量必须使用final修饰
+    jdk8默认添加，jdk7必须后动添加
 
  */
 public class Outer {
     int x = 2;
+    // final x = 2; // 默认已经自动添加
     public class Inner {
         int x = 1;
         public void inner() {
