aa31d42e8570ae2cdc2affe6eff3b6555b7bf19d
George <buubeebuu@126.com>
2021-04-13 19:06:39 +0800

    StringBuilder_构造方法

---
+++ 12_StringBuilder/src/A02_StringBuilder_构造方法/StringBuilderDemo01.java
@@ -0,0 +1,28 @@
+package A02_StringBuilder_构造方法;
+/*
+    StringBuilder构造方法：
+        public StringBuilder()：创建一个空白可变字符串对象，不含有任何内容
+        public StringBuilder(String str)：根据字符串的内容，来创建可变字符串对象
+ */
+public class StringBuilderDemo01 {
+    public static void main(String[] args) {
+
+        //public StringBuilder()：创建一个空白可变字符串对象，不含有任何内容
+        StringBuilder sb = new StringBuilder();
+        System.out.println("sb的内容:" + sb);
+        System.out.println("sb.length()的长度：" + sb.length());
+
+        //public StringBuilder(String str)：根据字符串的内容，来创建可变字符串对象
+        StringBuilder sb2 = new StringBuilder("Hello");
+        System.out.println("sb2:" + sb2);
+        System.out.println("sb2.length():" + sb2.length());
+
+        // 利用插入的方法添加
+//        sb = "Test"; // 错误 sb 为 StringBuilder是一个对象非字符串格式
+
+        if (sb.length() >= 0) {
+            sb.insert(0, "在指定的位置10，插入指定的字符串");// 在指定的位置10，插入指定的字符串
+        }
+        System.out.println("sb的内容:" + sb);
+    }
+}
