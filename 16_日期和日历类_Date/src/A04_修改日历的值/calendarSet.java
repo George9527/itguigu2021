efcab9c1af41c8fc997398cbf608b7e16d4fbcb0
George <buubeebuu@126.com>
2021-04-15 15:38:06 +0800

    修改日历的值

---
+++ 16_日期和日历类_Date/src/A04_修改日历的值/calendarSet.java
@@ -0,0 +1,29 @@
+package A04_修改日历的值;
+
+import java.util.Calendar;
+
+/*
+    void set() 修改日历的值
+        - set(int field,int value)field要修改的字段,value具体的数据
+        - set(int,int,int) 传递年月日
+ */
+public class calendarSet {
+
+    public static void main(String[] args) {
+
+        /**
+         * Calendar类的方法set()
+         * 设置日历字段
+         */
+
+        // 获取操作系统的时间
+        Calendar calendar = Calendar.getInstance();
+
+        // 重新设置当前时间 --- 注意这里的月份 系统月份范围 0-11
+        calendar.set(2021,6 - 1,30);
+        // 输出修改后的日期
+        System.out.println( calendar.get(Calendar.YEAR)+"年" + (calendar.get(Calendar.MONTH) +1)+"月" +
+                calendar.get(Calendar.DAY_OF_MONTH)+"日" + calendar.get(Calendar.HOUR_OF_DAY)+"点" +
+                calendar.get(Calendar.MINUTE)+"分"+calendar.get(Calendar.SECOND)+"秒");
+    }
+}
