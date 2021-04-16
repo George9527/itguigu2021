308c753f5fead99e8b9615ad3f61b3e76238d9e2
George <buubeebuu@126.com>
2021-04-16 19:23:36 +0800

    异常处理之throws

---
+++ 19_异常处理/src/A06_异常处理之throws/ExceptionDemo.java
@@ -0,0 +1,34 @@
+package A06_异常处理之throws;
+/*
+    throws 异常类名;
+    这个格式是跟在方法的括号后面的
+ */
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ExceptionDemo {
+    public static void main(String[] args) throws ParseException {
+        System.out.println("开始");
+//        method();
+        method2();
+
+        System.out.println("结束");
+    }
+
+    //编译时异常
+    public static void method2() throws ParseException {
+        String s = "2048-08-09";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date d = sdf.parse(s);
+        System.out.println(d);
+    }
+
+    //运行时异常
+    public static void method() throws ArrayIndexOutOfBoundsException {
+        int[] arr = {1, 2, 3};
+        System.out.println(arr[3]);
+    }
+
+}
