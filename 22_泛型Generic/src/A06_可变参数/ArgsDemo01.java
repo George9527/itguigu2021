35cd5bb52848fdf1bf42ae8657c0075b25ccdde5
George <buubeebuu@126.com>
2021-04-19 21:13:12 +0800

    可变参数

---
+++ 22_泛型Generic/src/A06_可变参数/ArgsDemo01.java
@@ -0,0 +1,36 @@
+package A06_可变参数;
+/*
+    测试类
+        麻烦：每多一个加数都要写一个新的方法
+ */
+public class ArgsDemo01 {
+    public static void main(String[] args) {
+        System.out.println(sum(10, 20));
+        System.out.println(sum(10, 20, 30));
+        System.out.println(sum(10, 20, 30, 40));
+
+        System.out.println(sum(10,20,30,40,50));
+        System.out.println(sum(10,20,30,40,50,60));
+        System.out.println(sum(10,20,30,40,50,60,70));
+    }
+
+//    public static int sum(int a, int b) {
+//        return a + b;
+//    }
+//
+//    public static int sum(int a, int b, int c) {
+//        return a + b + c;
+//    }
+//
+//    public static int sum(int a, int b, int c, int d) {
+//        return a + b + c + d;
+//    }
+
+    public static int sum(int... a) {
+        int sum = 0;
+        for (int i : a) {
+            sum += i;
+        }
+        return sum;
+    }
+}
