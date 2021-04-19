d55c385142945919ffa59b96a088918e15d6b7da
George <buubeebuu@126.com>
2021-04-19 23:52:44 +0800

    java

--- 00_Test/src/test/Something.java
+++ 00_Test/src/test/Something.java
@@ -2,14 +2,25 @@ package test;
 
 public class Something {
 
-    int i;
+    int a = 10;
+    int b = 9;
 
-    public void doSomething() {
-        System.out.println("i = " + i);
+    public void f(){
+        a = 0;
+        b = 8;
+
+        int[] c = {0};
+        g(b,c);
+        System.out.println(a + " " + b + " " + c[0]);
     }
-
+    public void g(int a, int[] c){
+        a = 1;
+        b = 1;
+        c[0] = 1;
+    }
     public static void main(String[] args) {
-        Something something = new Something();
-        something.doSomething();
+        Something t = new Something();
+        t.f();
     }
+
 }
