b2b3759780d1cd15a0bc0d74331ee891c169e13d
George <buubeebuu@126.com>
2021-04-21 19:02:30 +0800

    自定义异常

--- 19_异常处理/src/A07_自定义异常/ScoreException.java
+++ 19_异常处理/src/A07_自定义异常/ScoreException.java
@@ -1,6 +1,16 @@
 package A07_自定义异常;
 
-public class ScoreException extends Exception{
+/**
+ *  自定义的异常类
+ *    成绩负数的异常
+ *    继承哪个父类呢
+ *
+ *    自定义异常信息 :
+ *    1.继承父类 RuntimeException
+ *    2.带有String类型的构造方法 (String 异常信息)
+ */
+
+public class ScoreException extends RuntimeException {
 
     public ScoreException() {
     }
--- 19_异常处理/src/A07_自定义异常/TeacherTest.java
+++ 19_异常处理/src/A07_自定义异常/TeacherTest.java
@@ -2,6 +2,14 @@ package A07_自定义异常;
 
 import java.util.Scanner;
 
+/*
+      Java官方已经定义了大量的异常类,但是依然不够,
+      以后做项目的时候,会出现的异常,在JDK中没有定义的,需要我们自己定义异常
+
+  - 自定义异常,入伙,继承Exception或者RuntimeException
+  - 只有Exception和他的子类,才具有可抛出性
+  - 自定义的类中,构造方法,super调用父类构造方法,传递异常信息
+ */
 public class TeacherTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -12,7 +20,7 @@ public class TeacherTest {
         try {
             t.checkScore(score);
         } catch (ScoreException e) {
-//            e.printStackTrace();
+//            e.printStackTrace(); // 打印出错误的详细信息
         }
     }
 }
