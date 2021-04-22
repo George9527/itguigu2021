30648d230477c873f401a707338c6ce642ece950
George <buubeebuu@126.com>
2021-04-22 10:31:25 +0800

    线程控制_Daemon()

---
+++ 24_多线程一/src/A08_线程控制_Daemon/ThreadDaemon.java
@@ -0,0 +1,10 @@
+package A08_线程控制_Daemon;
+
+public class ThreadDaemon extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(getName() + ":" + i);
+        }
+    }
+}
---
+++ 24_多线程一/src/A08_线程控制_Daemon/ThreadDaemonDemo.java
@@ -0,0 +1,34 @@
+package A08_线程控制_Daemon;
+
+/*
+    方法名：
+        void setDaemon(boolean on)
+    说明：
+        将此线程标记为守护线程，
+        当运行的线程都是守护线程时，Java虚拟机将退出
+ */
+public class ThreadDaemonDemo {
+
+    public static void main(String[] args) {
+        ThreadDaemon td1 = new ThreadDaemon();
+        ThreadDaemon td2 = new ThreadDaemon();
+
+        td1.setName("关羽");
+        td2.setName("张飞");
+
+        //设置主线程为刘备
+        Thread.currentThread().setName("刘备");
+
+        //设置守护线程
+        td1.setDaemon(true);
+        td2.setDaemon(true);
+
+        td1.start();
+        td2.start();
+
+        for(int i=0; i<10; i++) {
+            System.out.println(Thread.currentThread().getName()+":"+i);
+        }
+    }
+
+}
