6af30ac982d5b047f2920a0a6d52ed9b67e89bae
George <buubeebuu@126.com>
2021-05-02 16:44:49 +0800

    字节输入流读取单个字节

--- 25_IO流对象/src/A07_字节输入流读取单个字节/readByte.java
+++ 25_IO流对象/src/A07_字节输入流读取单个字节/readByte.java
@@ -13,6 +13,13 @@ public class readByte {
         //创建字节输入流对象,绑定数据源文件
         FileInputStream fis = new FileInputStream("D:\\Project\\Java\\SGG2021\\25_IO流对象\\src\\file\\java.txt");
         //读取单个的字节
+
+        /*
+            fis.read()：读数据
+            by=fis.read()：把读取到的数据赋值给by
+            by != -1：判断读取到的数据是否是-1
+         */
+
         //循环读取,条件 read()!=-1就行
         int r = 0;
         while ((r = fis.read()) != -1) {
