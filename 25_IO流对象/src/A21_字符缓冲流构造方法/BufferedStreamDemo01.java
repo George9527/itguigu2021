c676c39fdf66e4a23eb4d49178f53cbe6e0ed6bf
George <buubeebuu@126.com>
2021-05-02 22:59:56 +0800

    字符缓冲流构造方法

---
+++ 25_IO流对象/src/A21_字符缓冲流构造方法/BufferedStreamDemo01.java
@@ -0,0 +1,45 @@
+package A21_字符缓冲流构造方法;
+
+import java.io.*;
+
+/**
+字符缓冲流介绍:
+    - BufferedWriter：
+        将文本写入字符输出流，缓冲字符，以提供单个字符，数组和字符串的高效写入，
+        可以指定缓冲区大小，或者可以接受默认大小。默认值足够大，可用于大多数用途
+
+    - BufferedReader：
+        从字符输入流读取文本，缓冲字符，以提供字符，数组和行的高效读取，
+        可以指定缓冲区大小，或者可以使用默认大小。 默认值足够大，可用于大多数用途
+
+构造方法：
+    BufferedWriter(Writer out) --- 创建字符缓冲输出流对象
+    BufferedReader(Reader in) --- 创建字符缓冲输入流对象
+ */
+public class BufferedStreamDemo01 {
+    public static void main(String[] args) throws IOException {
+        //BufferedWriter(Writer out)
+        BufferedWriter bw = new BufferedWriter(new FileWriter("myCharStream\\bw.txt"));
+        bw.write("hello\r\n");
+        bw.write("world\r\n");
+        bw.close();
+
+        // BufferedReader(Reader in) --- 创建字符缓冲输入流对象
+        BufferedReader br = new BufferedReader(new FileReader("myCharStream\\bw.txt"));
+
+        // 一次读取一个字符数据
+        //        int ch;
+//        while ((ch=br.read())!=-1) {
+//            System.out.print((char)ch);
+//        }
+
+        //一次读取一个字符数组数据
+        char[] chs = new char[1024];
+        int len;
+        while ((len=br.read(chs))!=-1) {
+            System.out.print(new String(chs,0,len));
+        }
+
+        br.close();
+    }
+}
